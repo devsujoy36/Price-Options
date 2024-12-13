8ecfe3e8219bad6abe4358a5d04853dc8a903165
unknown <sujoydaspc2023@gmail.com>
2024-12-13 12:59:24 +0600

    adding charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar/>
       <PriceOptions/>
+      <LineChart/>
 
     </>
   )
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -2,23 +2,26 @@
 import { LineChart as LChart, Line } from 'recharts';
 
 const LineChart = () => {
-    const mathMarks = [
-        { id: 1, name: "Alice", marks: 78 },
-        { id: 2, name: "Bob", marks: 85 },
-        { id: 3, name: "Charlie", marks: 92 },
-        { id: 4, name: "David", marks: 88 },
-        { id: 5, name: "Emma", marks: 74 },
-        { id: 6, name: "Fiona", marks: 81 },
-        { id: 7, name: "George", marks: 95 },
-        { id: 8, name: "Hannah", marks: 69 },
-        { id: 9, name: "Ian", marks: 87 },
-        { id: 10, name: "Julia", marks: 80 }
+    const studentsMarks = [
+        { id: 1, name: "Alice", math: 78, physics: 85, chemistry: 88 },
+        { id: 2, name: "Bob", math: 85, physics: 80, chemistry: 82 },
+        { id: 3, name: "Charlie", math: 92, physics: 89, chemistry: 91 },
+        { id: 4, name: "David", math: 88, physics: 84, chemistry: 79 },
+        { id: 5, name: "Emma", math: 74, physics: 78, chemistry: 76 },
+        { id: 6, name: "Fiona", math: 81, physics: 87, chemistry: 85 },
+        { id: 7, name: "George", math: 91, physics: 92, chemistry: 94 },
+        { id: 8, name: "Hannah", math: 69, physics: 73, chemistry: 70 },
+        { id: 9, name: "Ian", math: 87, physics: 88, chemistry: 83 },
+        { id: 10, name: "Julia", math: 80, physics: 81, chemistry: 79 }
       ];
       
+      
   return (
-    <div>
-        <LChart width={500} height={400} data={mathMarks}>
-            <Line></Line>
+    <div className="max-w-screen-2xl my-10 md:mx-auto mx-16">
+        <LChart width={800} height={400} data={studentsMarks}>
+            <Line stroke='red' dataKey="math"></Line>
+            <Line stroke='blue' dataKey="physics"></Line>
+            <Line stroke='green' dataKey="chemistry"></Line>
         </LChart>
     </div>
   )
